d12a69d9f7ea2e564e70aaca4d7604b5c76048d9
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-16 16:36:49 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Edwin Przeźwiecki Jr. on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
