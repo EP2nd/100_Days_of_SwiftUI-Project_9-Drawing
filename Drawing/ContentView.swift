af4f04d6f9926e5e7fbf20968e043f3892bc50be
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-18 20:57:58 +0100

    Solutions to all three challenges

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,20 +7,112 @@
 
 import SwiftUI
 
+struct ArrowHead: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct ArrowShaft: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: 200))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct ColorCyclingRectangle: View {
+    
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var shaftThickness = 10.0
+    
+    @State private var colorCycle = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(spacing: -100) {
+                ArrowHead()
+                    .fill(.gray)
+                    .frame(width: 100, height: 100)
+                
+                ArrowShaft()
+                    .stroke(.brown, lineWidth: shaftThickness)
+                    .frame(width: 100, height: 100)
+            }
+            .padding(200)
+            
+            Spacer()
+            
+            Slider(value: $shaftThickness, in: 1...20)
+                .padding()
         }
-        .padding()
+        
+        VStack {
+            ZStack {
+                ColorCyclingRectangle(amount: colorCycle)
+                    .frame(width: 300, height: 300)
+            }
+            .padding(200)
+          
+            Spacer()
+            
+            Slider(value: $colorCycle)
+                .padding([.bottom, .horizontal])
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
 }
