4b11485ff49f0f226daf717a4c56240ebedf26f8
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-19 14:41:59 +0100

    Corrections and improvements

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,36 +7,68 @@
 
 import SwiftUI
 
+/// Challenge 1:
 struct ArrowHead: Shape {
+    
+    /// Challenge 2:
+    var headWidth = 0.5
+    
+    /// Challenge 2:
+    var animatableData: Double {
+        get { headWidth }
+        set { headWidth = newValue }
+    }
+    
     func path(in rect: CGRect) -> Path {
         
-        var path = Path()
+        /// Challenge 2:
+        let headWidth = rect.width * headWidth / 2
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        return path
+        /// Challenge 2:
+        return Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX + headWidth, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX - headWidth, y: rect.maxY))
+            path.closeSubpath()
+        }
     }
 }
-
+    
+/// Challenge 1:
 struct ArrowShaft: Shape {
+    
+    /// Challenge 2:
+    var shaftThickness = 10.0
+    
+    /// Challenge 2:
+    var animatableData: Double {
+        get { shaftThickness }
+        set { shaftThickness = newValue}
+    }
+    
     func path(in rect: CGRect) -> Path {
         
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: 200))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-        
-        return path
+        /// Challenge 2:
+        return Path { path in
+            path.move(to: CGPoint(x: rect.midX + shaftThickness, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.midX + shaftThickness, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX - shaftThickness, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX - shaftThickness, y: rect.minY))
+            path.closeSubpath()
+        }
     }
 }
 
+/// Challenge 3:
 struct ColorCyclingRectangle: View {
     
     var amount = 0.0
     var steps = 100
+    
+    var gradientStartX = 0.5
+    var gradientStartY = 0.0
+    var gradientEndX = 0.5
+    var gradientEndY = 1.0
     
     var body: some View {
         ZStack {
@@ -49,8 +81,8 @@ struct ColorCyclingRectangle: View {
                                 color(for: value, brightness: 1),
                                 color(for: value, brightness: 0.5)
                             ]),
-                            startPoint: .top,
-                            endPoint: .bottom
+                            startPoint: UnitPoint(x: gradientStartX, y: gradientStartY),
+                            endPoint: UnitPoint(x: gradientEndX, y: gradientEndY)
                         ),
                         lineWidth: 2
                     )
@@ -73,40 +105,60 @@ struct ColorCyclingRectangle: View {
 
 struct ContentView: View {
     
-    @State private var shaftThickness = 10.0
+    /// Challenge 2:
+    @State private var headWidth = 0.5
+    @State private var shaftThickness = 20.0
     
+    @State private var gradientStartX = 0.5
+    @State private var gradientStartY = 0.0
+    @State private var gradientEndX = 0.5
+    @State private var gradientEndY = 1.0
+    
+    /// Challenge 3:
     @State private var colorCycle = 0.0
     
     var body: some View {
+        /// Challenge 1 and 2:
         VStack {
-            VStack(spacing: -100) {
-                ArrowHead()
+            VStack(spacing: 0) {
+                ArrowHead(headWidth: headWidth)
                     .fill(.gray)
-                    .frame(width: 100, height: 100)
                 
-                ArrowShaft()
-                    .stroke(.brown, lineWidth: shaftThickness)
-                    .frame(width: 100, height: 100)
+                ArrowShaft(shaftThickness: shaftThickness)
+                    .fill(.brown)
             }
-            .padding(200)
-            
-            Spacer()
-            
-            Slider(value: $shaftThickness, in: 1...20)
-                .padding()
+            .onTapGesture {
+                withAnimation {
+                    headWidth = Double.random(in: 0.2...0.8)
+                    shaftThickness = Double.random(in: 10...20)
+                }
+            }
         }
-        
+        /// Challenge 3:
         VStack {
-            ZStack {
-                ColorCyclingRectangle(amount: colorCycle)
+            VStack {
+                ColorCyclingRectangle(amount: colorCycle, gradientStartX: gradientStartX, gradientStartY: gradientStartY, gradientEndX: gradientEndX, gradientEndY: gradientEndY)
                     .frame(width: 300, height: 300)
             }
-            .padding(200)
-          
-            Spacer()
+            .padding()
             
-            Slider(value: $colorCycle)
-                .padding([.bottom, .horizontal])
+            Group {
+                Text("Color:")
+                Slider(value: $colorCycle)
+                
+                Text("Gradient's X start point:")
+                Slider(value: $gradientStartX)
+                
+                Text("Gradient's Y start point:")
+                Slider(value: $gradientStartY)
+                
+                Text("Gradient's X end point:")
+                Slider(value: $gradientEndX)
+                
+                Text("Gradient's Y end point:")
+                Slider(value: $gradientEndY)
+            }
+            .padding(.horizontal)
         }
     }
 }
